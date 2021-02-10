2190e465d840dd5252c78d5198dec88165e1612f
DiegoBrito <dibbrito@gmail.com>
2021-02-10 15:57:56 -0300

    Add jpa package for tests

---
+++ src/main/java/com/algaworks/algafood/jpa/InclusaoCozinhaMain.java
@@ -0,0 +1,28 @@
+package com.algaworks.algafood.jpa;
+
+import com.algaworks.algafood.AlgafoodApiApplication;
+import com.algaworks.algafood.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class InclusaoCozinhaMain {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+        Cozinha c1 = new Cozinha();
+        c1.setName("Brasileira");
+        Cozinha c2 = new Cozinha();
+        c2.setName("Japonesa");
+
+        cadastroCozinha.adicionar(c1);
+        cadastroCozinha.adicionar(c2);
+
+    }
+}
