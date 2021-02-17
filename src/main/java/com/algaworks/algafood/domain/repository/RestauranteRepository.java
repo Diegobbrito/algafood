4163378758e504c88780d1e8ad0561ccf3cd9f85
DiegoBrito <dibbrito@gmail.com>
2021-02-17 15:00:17 -0300

    Add JPQL for restaurante with @Query

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -2,8 +2,20 @@ package com.algaworks.algafood.domain.repository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+    @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+    List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
+
+    List<Restaurante> findByTaxaFreteBetween (BigDecimal taxaInitial, BigDecimal taxaFinal);
+    List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
+    List<Restaurante> findTop2ByNomeContaining(String nome);
+    int countByCozinhaId(Long cozinha);
 }
