a144f1d4530ddf7630c04a25c55465f09ee51ccb
DiegoBrito <dibbrito@gmail.com>
2021-02-21 21:50:35 -0300

    Updated findAll of Restaurante

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.domain.repository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -11,9 +12,11 @@ import java.util.List;
 @Repository
 public interface RestauranteRepository extends CustomJpaRepository<Restaurante, Long>, RestauranteRepositoryQueries, JpaSpecificationExecutor<Restaurante> {
 
+    @Query("from Restaurante r join r.cozinha left join fetch r.formasPagamento")
+    List<Restaurante> findAll();
+
 //    @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
     List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
-
     List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
     List<Restaurante> findByTaxaFreteBetween (BigDecimal taxaInitial, BigDecimal taxaFinal);
     List<Restaurante> findTop2ByNomeContaining(String nome);
