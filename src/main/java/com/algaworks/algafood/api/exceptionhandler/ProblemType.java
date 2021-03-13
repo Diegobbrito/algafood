d289be4bd698dd24a80fdc511e00539e26ed2c02
DiegoBrito <dibbrito@gmail.com>
2021-03-13 20:24:44 -0300

    Update all ApiExceptionHandler methods using RFC 7807

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -4,12 +4,14 @@ import lombok.Getter;
 
 @Getter
 public enum ProblemType {
-    ENTIDADE_NAO_ENCONTRADA("/endtidade-nao-encontrada", "Entidade não encontrada");
+    ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada"),
+    ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
+    ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio");
 
     private String title;
     private String uri;
 
-    ProblemType(String path, String title){
+    ProblemType(String path, String title) {
         this.uri = "https://algafood.com.br" + path;
         this.title = title;
     }
